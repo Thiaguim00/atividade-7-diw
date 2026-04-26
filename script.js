9e9bcd50c1d5db27e11a3836d514c8ed03307f5f
Thiaguim00 <thiagohenque10050@gmail.com>
2026-04-26 18:09:28 -0300

    att

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("TESTE FUNCIONANDO");
+alert("JS rodou!");
+
+let x, y, z, u, i, o;
+
+u = 0;
+
+let name = prompt("Escreva seu nome: ");
+
+x = Number(prompt("Escreva sua renda mensal: "));
+while (isNaN(x)) {
+    x = Number(prompt("Valor inválido. Escreva sua renda mensal: "));
+}
+
+y = Number(prompt("Quantidade de despesas mensais (até 5): "));
+while (isNaN(y)) {
+    y = Number(prompt("Valor inválido. Digite a quantidade de despesas: "));
+}
+
+if (y > 5) {
+    y = 5;
+}
+if (y < 1) {
+    y = 1;
+}
+
+for (let i = 1; i <= y; i++) {
+    z = Number(prompt("Escreva o valor da despesa " + i + ": "));
+
+    while (isNaN(z)) {
+        z = Number(prompt("Valor inválido. Digite novamente a despesa " + i + ": "));
+    }
+
+    u += z;
+}
+
+let mensagem
+
+if (u > x) {
+    mensagem = "Infelizmente, " + name + ", suas despesas são maiores que sua renda mensal. Tente reduzir seus gastos.";
+}
+else {    o = x - u;
+    mensagem = "Parabéns, " + name + "! Suas despesas são menores que sua renda mensal. Você tem um saldo positivo de " + o + " reais.";
+}
+
+let resultado =
+"Nome: " + name + "\n" +
+"Renda: R$ " + x.toFixed(2) + "\n" +
+"Despesas: R$ " + u.toFixed(2) + "\n" +
+"Sobra: R$ " + (x - u).toFixed(2) + "\n" +
+mensagem;
+
+alert(resultado);
+console.log(resultado);
